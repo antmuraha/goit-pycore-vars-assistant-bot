3a0dee158bea070826ca58a2576a8a0ec035feba
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-11-07 11:46:39 +0100

    add edit method

--- assistant_bot/record_note.py
+++ assistant_bot/record_note.py
@@ -5,7 +5,7 @@ class RecordNote:
     A class to store information about a note, including text.
     '''
 
-    def __init__(self, title, text):
+    def __init__(self, text):
         self.text = FieldText(text)
 
     def __str__(self):
@@ -14,5 +14,6 @@ class RecordNote:
     def __repr__(self):
         return f"RecordNote(text={self.text[:30]!r})"
     
-    
+    def edit_text(self, new_text):
+        self.text = new_text
     
