3827d5bba9e42fd443c28974426af13a401835ec
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-11-07 11:27:31 +0100

    fix methods

--- assistant_bot/record_contact.py
+++ assistant_bot/record_contact.py
@@ -1,6 +1,6 @@
 from fields import FieldName, FieldPhone, FieldBirthday
-# from fields import FieldName, FieldPhone, FieldBirthday, FieldAddress, FieldEmail
-
+from fields import FieldName, FieldPhone, FieldBirthday, FieldAddress, FieldEmail
+from datetime import datetime
 
 
 class RecordContact:
@@ -12,10 +12,8 @@ class RecordContact:
         self.name = FieldName(name)
         self.phones: list[FieldPhone] = []
         self.birthday: FieldBirthday = None
-        self.email = None
-        self.address = None
-        # self.email: FieldEmail = None
-        # self.address: FieldAddress = None
+        self.email: FieldEmail = None
+        self.address: FieldAddress = None
 
     def __str__(self):
         birthday_str = f" ({self.birthday})" if self.birthday else ''
@@ -28,47 +26,47 @@ class RecordContact:
         if not self.find_phone(phone):
             self.phones.append(FieldPhone(phone))
         else:
-            raise NotImplementedError 
+            raise ValueError 
 
     def add_birthday(self, birthday: str):
         self.birthday = FieldBirthday(birthday)
         
-    #TODO show_birthday()    
+    def show_birthday(self):
+        if self.birthday:
+            return self.birthday.strftime("%d-%m-%Y")
+        else:
+            return "You haven't added a birthday yet."
 
     def remove_phone(self, phone: str):
         self.phones = list(filter(lambda p: p.value != phone, self.phones))
 
-    def edit_phone(self, phone: str, new_phone: str):
+    def edit_phone(self, phone: FieldPhone, new_phone: FieldPhone):
         record = next(
             (x for x in self.phones if x.value == phone), None)
 
         if record:
             record.value = new_phone
 
-    def find_phone(self, phone: str) -> FieldPhone | None:
+    def find_phone(self, phone: FieldPhone) -> FieldPhone | None:
         record = next(
             (x for x in self.phones if x.value == phone), None)
 
         return record
     
-    def add_email(self, email): # email: FieldEmail
-        raise NotImplementedError
-        # self.email = FieldEmail(email)
+    def add_email(self, email: FieldEmail):  
+        self.email = FieldEmail(email)
 
-    def edit_email(self, new_email): # new_email: FieldEmail
-        raise NotImplementedError
-        # self.email = FieldEmail(new_email)
+    def edit_email(self, new_email: FieldEmail): 
+        self.email = FieldEmail(new_email)
 
     def remove_email(self):
         self.email = None
 
-    def add_address(self, address): # address: FieldAddress
-        raise NotImplementedError
-        # self.address = FieldAddress(address)
+    def add_address(self, address: FieldAddress): 
+        self.address = FieldAddress(address)
 
-    def edit_address(self, new_address): # new_address: FieldAddress
-        raise NotImplementedError
-        # self.address = FieldAddress(new_address)
+    def edit_address(self, new_address: FieldAddress): 
+        self.address = FieldAddress(new_address)
 
     def remove_address(self):
         self.address = None
