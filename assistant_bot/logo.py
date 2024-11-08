52b25205b1d64b05117e4be1645624af4ddf23cf
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-11-08 16:37:42 +0100

    install package

---
+++ assistant_bot/logo.py
@@ -0,0 +1,14 @@
+from pyfiglet import Figlet
+f = Figlet(font='slant')
+print(f.renderText('vars'))
+print(f.renderText('VARS'))
+
+# f = Figlet(font='acrobatic')
+# f = Figlet(font='doom')
+# f = Figlet(font='nipples')
+# f = Figlet(font='ogre')
+# f = Figlet(font='rev')
+# f = Figlet(font='slant')
+# f = Figlet(font='starwars')
+
+
